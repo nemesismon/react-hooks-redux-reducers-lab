6d9b09931a1283bd033b9a23849c768c0ac1afd6
Michael <michael.w.kolb@gmail.com>
2023-02-03 14:19:39 -0500

    Completed assignment

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,15 @@
-export function manageFriends(state, action) {
+const friends = []
+
+export function manageFriends(state = {friends}, action) {
   // your code here
+
+  switch (action.type) {
+    case "friends/add":
+      state.friends.push(action.payload)
+      return { friends: state.friends }
+    case "friends/remove":
+      return { friends: state.friends.filter((friend) => friend.id !== action.payload)}
+    default:
+        return state
+  }
 }
